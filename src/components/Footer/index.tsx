7c07adb2c768f7c6a8d0f1227735025a4a043140
FengXuan <xuanjianfeng@163.com>
2024-10-22 05:57:22 +0800

    update src/components/Footer/index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -145,9 +145,6 @@ const Footer: React.FC = () => {
       </InfoPanel>
 
       <footer className="mb-1 mt-4 flex w-full items-center justify-center gap-2.5 text-sm ease-in" onClick={(e) => e.currentTarget.blur()}>
-        <a href="https://github.com/fengxuan/qwerty-learner" target="_blank" rel="noreferrer" aria-label="前往 GitHub 项目主页">
-          <IconGithub fontSize={15} className="text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100" />
-        </a>
         <a
           className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           href="https://qwerty.kaiyi.cool"
@@ -159,7 +156,9 @@ const Footer: React.FC = () => {
         </a>
       </footer>
       <footer className="mb-1 mt-4 flex w-full items-center justify-center gap-2.5 text-sm ease-in" onClick={(e) => e.currentTarget.blur()}>
-
+        <a href="https://github.com/fengxuan/qwerty-learner" target="_blank" rel="noreferrer" aria-label="前往 GitHub 项目主页">
+          <IconGithub fontSize={15} className="text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100" />
+        </a>
         <a
           className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           href="https://babelchat.lat"
